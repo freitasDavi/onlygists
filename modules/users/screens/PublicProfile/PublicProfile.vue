5dfa9849bb6adeb6333b620bf742f636e72f0d5a
davi <davi.frrs@outlook.com>
2024-03-08 09:42:04 -0300

    feat: report widgets

--- modules/users/screens/PublicProfile/PublicProfile.vue
+++ modules/users/screens/PublicProfile/PublicProfile.vue
@@ -1,7 +1,18 @@
 <script setup lang="ts">
 import PublicHeadline from '@/modules/users/components/PublicHeadline/PublicHeadline.vue';
+import WidgetGroup from '@/modules/reports/components/Widget/Group/Group.vue'
+import WidgetGroupLoader from '@/modules/reports/components/Widget/Group/Loader.vue'
+import WidgetCondensed from '@/modules/reports/components/Widget/Condensed/Condensed.vue'
+
 </script>
 
 <template>
     <PublicHeadline />
+    <WidgetGroup>
+        <WidgetGroupLoader :loading="true" :amount="3">
+            <WidgetCondensed :value="10" label="Gists do total" />
+            <WidgetCondensed :value="5" label="Gists do gratuitos" />
+            <WidgetCondensed :value="5" label="Gists do pagos" />
+        </WidgetGroupLoader>
+    </WidgetGroup>
 </template>
