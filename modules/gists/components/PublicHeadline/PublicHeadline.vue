304192bad5936b1ca8b40e89e1f235e9c8441d69
Davi PC <davi.frrs@gmail.com>
2024-03-12 21:10:52 -0300

    feat: code snippet

--- modules/gists/components/PublicHeadline/PublicHeadline.vue
+++ modules/gists/components/PublicHeadline/PublicHeadline.vue
@@ -28,7 +28,7 @@
         <p class="font-[Inter] font-regular text-gray-500 text-sm md:text-base">
             <i class="pi pi-star"></i>
             by
-            <NuxtLink :to="`/${props.author}`">@{{ props.author }}</NuxtLink>
+            <NuxtLink :to="`/${props.author}`" class="text-primary-500">@{{ props.author }}</NuxtLink>
         </p>
     </div>
 </template>
--- modules/gists/screens/MoreDetail/MoreDetail.vue
+++ modules/gists/screens/MoreDetail/MoreDetail.vue
@@ -1,7 +1,11 @@
 <script setup lang="ts">
 import PublicHeadline from '@/modules/gists/components/PublicHeadline/PublicHeadline.vue';
+import GistCodeSnippet from '@/modules/gists/components/GistCodeSnippet/GistCodeSnippet.vue';
 </script>
 
 <template>
     <PublicHeadline />
+    <GistCodeSnippet 
+
+    />
 </template>
