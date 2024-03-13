91b032a9c2aa56099417bcb652cde2d15dc2f71a
Davi PC <davi.frrs@gmail.com>
2024-03-12 21:35:16 -0300

    feat: modals

--- modules/gists/screens/MoreDetail/MoreDetail.vue
+++ modules/gists/screens/MoreDetail/MoreDetail.vue
@@ -1,11 +1,33 @@
 <script setup lang="ts">
 import PublicHeadline from '@/modules/gists/components/PublicHeadline/PublicHeadline.vue';
 import GistCodeSnippet from '@/modules/gists/components/GistCodeSnippet/GistCodeSnippet.vue';
+import LazyDialogPaymentSuccess from '@/modules/payments/components/DialogPaymentSuccess/DialogPaymentSuccess.vue';
+import LazyDialogPaymentError from '@/modules/payments/components/DialogPaymentError/DialogPaymentError.vue';
+
+const route = useRoute()
+const isPaymentSuccessfully = ref<boolean>(false);
+const isPaymentFailed = ref<boolean>(false)
+
+
+
+onMounted(() => {
+    const { success_payment, fail_payment } = route.query
+
+    if (success_payment) {
+        isPaymentSuccessfully.value = true;
+    }
+
+    if (fail_payment) {
+        isPaymentFailed.value = true;
+    }
+})
+
+
 </script>
 
 <template>
     <PublicHeadline />
-    <GistCodeSnippet 
-
-    />
+    <GistCodeSnippet />
+    <LazyDialogPaymentSuccess v-model:visible="isPaymentSuccessfully"  />
+    <LazyDialogPaymentError v-model:visible="isPaymentFailed"  />
 </template>
